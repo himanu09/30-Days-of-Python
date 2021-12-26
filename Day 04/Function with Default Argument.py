dd7a18f61636a6623f68c7bf5768c4834c1d6ed1
himanu09 <hhimanshurajore@gmail.com>
2021-12-26 16:42:27 +0530

    Create Function with Default Argument.py

---
+++ Day 04/Function with Default Argument.py
@@ -0,0 +1,6 @@
+# Function with Default Argument
+
+def employee_details(name,salary="9000"):
+    print("Name:",name,",Salary:",salary)
+employee_details("Himanshu",50000)
+employee_details("Mohan")

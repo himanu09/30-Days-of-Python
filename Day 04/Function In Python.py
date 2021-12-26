48f0209109cb40e4401dbfd6141eff6be3f06ed9
himanu09 <hhimanshurajore@gmail.com>
2021-12-26 16:42:32 +0530

    Create Function In Python.py

---
+++ Day 04/Function In Python.py
@@ -0,0 +1,9 @@
+# Create a Function in Pyton
+
+a=input("Enter You'r name here: \n")
+b=int(input("Enter You'r age here: \n"))
+def details(a,b):
+    print("Here is Details of You !,", a ,b)
+
+details(a,b)
+

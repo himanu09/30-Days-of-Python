11f70b9c792bd750eaf7f66d5ce6d40315f52e8a
himanu09 <hhimanshurajore@gmail.com>
2021-12-29 14:31:27 +0530

    Update SSP Game.py

--- Game/SSP Game.py
+++ Game/SSP Game.py
@@ -3,7 +3,7 @@ import random
 def Mygame(computer,you):
     if computer==you:
         print("The Game is Tie!")
-    elif computer=="S":
+    elif computer=="r":
         if you=="p":
             print("You Won!")
         elif you=="s":
@@ -11,19 +11,19 @@ def Mygame(computer,you):
     elif computer=="p":
         if you=="s":
             print("You Won!")
-        elif you=="S":
+        elif you=="r":
             print("You Lose!")
     elif computer=="s":
-        if you=="S":
+        if you=="r":
             print("You Won!")
         elif you=="p":
             print("You Lose!")
 
 computer=random.randint(1,3)
-print("Computer Turn: Choose one from it Stone(S), Paper(p), Scissor(s):")
-you=input("You'r Turn: Choose one from it Stone(S), Paper(p), Scissor(s):")
+print("Computer Turn: Choose one from it Rock(r), Paper(p), Scissor(s):")
+you=input("You'r Turn: Choose one from it Rock(r), Paper(p), Scissor(s):")
 if computer==1:
-    computer="S"
+    computer="r"
 elif computer==2:
     computer="p"
 elif computer==3:

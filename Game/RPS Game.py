a8beb09717ac6e9a17fbfc7f684677ea358b1df7
himanu09 <hhimanshurajore@gmail.com>
2021-12-29 14:34:01 +0530

    Create RPS Game.py

---
+++ Game/RPS Game.py
@@ -0,0 +1,35 @@
+import random
+
+def Mygame(computer,you):
+    if computer==you:
+        print("The Game is Tie!")
+    elif computer=="r":
+        if you=="p":
+            print("You Won!")
+        elif you=="s":
+            print("You Lose!")
+    elif computer=="p":
+        if you=="s":
+            print("You Won!")
+        elif you=="r":
+            print("You Lose!")
+    elif computer=="s":
+        if you=="r":
+            print("You Won!")
+        elif you=="p":
+            print("You Lose!")
+
+computer=random.randint(1,3)
+print("Computer Turn: Choose one from it Rock(r), Paper(p), Scissor(s):")
+you=input("You'r Turn: Choose one from it Rock(r), Paper(p), Scissor(s):")
+if computer==1:
+    computer="r"
+elif computer==2:
+    computer="p"
+elif computer==3:
+    computer="s"
+
+print(f"Computer Choose {computer}")
+print(f"You Choose {you}")
+
+Mygame(computer,you)
